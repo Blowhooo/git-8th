1f70ac0caa7c7c2ec9cbefbbdf7f959a3e7d9cdf
Blowhooo <blowhooo@gmail.com>
2024-12-02 21:44:56 +0900

    여덟번째 연습

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,25 +1,23 @@
 'use strict';
 
-class UserStorage {
-  static #users = {
-    id : ["login1", "login2", "login3"],
-    psword : ["1234", "5678", "9101"],
-    name : ["윤준후1", "윤준후2", "윤준후3"]
-  };
+const fs = require('fs').promises;
 
-  static getUsers(...fields){
-    const users = this.#users;
+class UserStorage {
+
+  static #getUsers(isAll, data, ...fields){
+    const users = JSON.parse(data);
+    if(isAll) return users;
     const newUsers = fields.reduce((newUsers, field) => {
       if(users.hasOwnProperty(field)){
-        newUsers[field] = users[field]
+        newUsers[field] = users[field];
       }
       return newUsers;
     }, {});
     return newUsers;
   };
 
-  static getUserInfo(id){
-    const users = this.#users;
+  static #getUserInfo(id, data){
+    const users = JSON.parse(data);
     const idx = users.id.indexOf(id);
     const userKeys = Object.keys(users);
     const userInfo = userKeys.reduce((newUsers, info) => {
@@ -27,6 +25,35 @@ class UserStorage {
       return newUsers;
     }, {});
     return userInfo;
+  }
+
+  static getUsers(isAll, ...fields){
+    return fs.readFile('./src/databases/users.json')
+    .then(data => {
+      return this.#getUsers(isAll, data, ...fields)
+    })
+    .catch(console.error)
+  };
+
+  static getUserInfo(id){
+    return fs.readFile('./src/databases/users.json')
+    .then(data => {
+      return this.#getUserInfo(id, data);
+    })
+    .catch(console.error);
+  }
+
+  static async save(userInfo){
+    const users = await this.getUsers(true);
+
+    if(users.id.includes(userInfo.id)){
+      throw "이미 존재하는 아이디입니다";
+    }
+    users.id.push(userInfo.id);
+    users.psword.push(userInfo.psword);
+    users.name.push(userInfo.name);
+    fs.writeFile('./src/databases/users.json', JSON.stringify(users));
+    return {success: true, msg : "회원가입을 완료했습니다."}
   }
 }
 
